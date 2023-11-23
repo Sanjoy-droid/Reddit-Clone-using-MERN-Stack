cd41e181b6d70401eca08d1116f073ee7c0bb201
Sanjoy Kumar Guin <guinsanjoy34@gmail.com>
2023-11-23 20:14:23 +0530

    authtoken generated

--- backend/readme.md
+++ backend/readme.md
@@ -1,1 +1,11 @@
-install bcryptjs "npm i bcryptjs"
+install and import bcryptjs
+
+"npm i bcryptjs"
+const bcrypt = require("bcryptjs");
+
+setup the hashing passwords to the database
+
+install and import jsonwebtoken
+
+"npm i jsonwebtoken"
+var jwt = require('jsonwebtoken');
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,6 +3,11 @@ const User = require("../models/User");
 const router = express.Router();
 const { body, validationResult } = require("express-validator");
 
+const bcrypt = require("bcryptjs");
+var jwt = require("jsonwebtoken");
+
+const JWT_SECRET = "SanjoyRedditClone";
+
 // Create a User using POST "/api/auth/createuser". No login required
 
 router.post(
@@ -27,12 +32,24 @@ router.post(
           .status(400)
           .json({ error: "Sorry a user with this email already exists!!!" });
       }
+
+      const salt = await bcrypt.genSalt(10);
+      secPass = await bcrypt.hash(req.body.password, salt);
+
       user = await User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: secPass,
       });
-      res.json(user);
+
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authToken = jwt.sign(data, JWT_SECRET);
+
+      res.json({ authToken });
     } catch (error) {
       console.log(error.message);
       res.status(500).send("Some Error occurred");
